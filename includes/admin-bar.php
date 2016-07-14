4858f237ed8eb1fde0306e140eba83f9aff32152
Chris Klosowski <cklosowski@gmail.com>
2016-07-14 09:52:46 -0700

    Add option to menu to delete all licenses when SL enabled

--- includes/admin-bar.php
+++ includes/admin-bar.php
@@ -24,10 +24,11 @@ class EDD_DT_Admin_Bar {
 		// Add items to the admin bar
 		add_action( 'admin_bar_menu', array( $this, 'blog_id' ), 999 );
 		add_action( 'admin_bar_menu', array( $this, 'empty_cart' ), 999 );
+		add_action( 'admin_bar_menu', array( $this, 'delete_licenses' ), 999 );
 
 		// Capture actions from links added to the menu bar, if needed
 		add_action( 'init', array( $this, 'process_empty_cart' ) );
-
+		add_action( 'init', array( $this, 'process_delete_licenses' ) );
 	}
 
 	public function blog_id( $wp_admin_bar ) {
@@ -44,9 +45,16 @@ class EDD_DT_Admin_Bar {
 	}
 
 	public function empty_cart( $wp_admin_bar ) {
-		$args = array(
+		$title = __( 'Empty Cart', 'edd-dev-tools' );
+		$count = count( edd_get_cart_contents() );
+
+		if ( ! empty( $count ) ) {
+			$title .= '(' . $count . ')';
+		}
+
+		$args  = array(
 			'id'    => 'edd_empty_cart',
-			'title' => 'Empty Cart',
+			'title' => $title,
 			'href'  => add_query_arg( 'empty_cart', true ),
 		);
 		$wp_admin_bar->add_node( $args );
@@ -58,6 +66,36 @@ class EDD_DT_Admin_Bar {
 				edd_empty_cart();
 
 				wp_redirect( remove_query_arg( 'empty_cart' ) );
+				exit;
+			}
+		}
+	}
+
+	public function delete_licenses( $wp_admin_bar ) {
+		if ( function_exists( 'edd_software_licensing' ) ) {
+			$title = __( 'Delete Licenses', 'edd-dev-tools' );
+
+			$args  = array(
+				'id'    => 'edd_delete_licenses',
+				'title' => $title,
+				'href'  => add_query_arg( 'delete_licenses', true ),
+			);
+			$wp_admin_bar->add_node( $args );
+		}
+	}
+
+	public function process_delete_licenses() {
+		global $wpdb;
+		if ( isset( $_GET['delete_licenses'] ) ) {
+			if ( $_GET['delete_licenses'] == '1' ) {
+				$post_ids = $wpdb->get_col( "SELECT ID FROM $wpdb->posts WHERE post_type = 'edd_license'" );
+				if ( ! empty( $post_ids ) ) {
+					$post_ids = implode( ',', $post_ids );
+					$wpdb->query( "DELETE FROM $wpdb->posts WHERE ID IN ({$post_ids})" );
+					$wpdb->query( "DELETE FROM $wpdb->postmeta WHERE post_id IN ({$post_ids})" );
+				}
+
+				wp_redirect( remove_query_arg( 'delete_licenses' ) );
 				exit;
 			}
 		}
