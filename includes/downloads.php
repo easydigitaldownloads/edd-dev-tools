f54ef674fc005c0a6ae9526042bdeb92789e3a4e
Chris Klosowski <cklosowski@gmail.com>
2017-11-02 16:46:17 -0700

    Render download post meta when viewing a download

--- includes/downloads.php
+++ includes/downloads.php
@@ -26,10 +26,12 @@ class EDD_DT_Downloads {
 
 	private function hooks() {
 		add_action( 'add_meta_boxes', array( $this, 'register_meta_boxes' ) );
+		add_action( 'edd_view_order_details_main_after', array( $this, 'payment_post_meta' ), 99, 1 );
 	}
 
 	public function register_meta_boxes() {
-		add_meta_box( 'edd_dt_recent_payments', __( 'Recent Payments (Dev Tools)', 'edd-dev-tools' ), array( $this, 'render_metabox' ), 'download', 'normal', 'core' );
+		add_meta_box( 'edd_dt_recent_payments', __( 'Recent Payments (Dev Tools)', 'edd-dev-tools' ), array( $this, 'render_metabox' )      , 'download', 'normal', 'core' );
+		add_meta_box( 'edd_dt_download_meta'  , __( 'Download Meta (Dev Tools)', 'edd-dev-tools' )  , array( $this, 'render_download_meta' ), 'download', 'normal', 'core' );
 	}
 
 	public function render_metabox() {
@@ -72,6 +74,62 @@ class EDD_DT_Downloads {
 					<?php endif; ?>
 				</tbody>
 			</table>
+		</div>
+		<?php
+	}
+
+	public function render_download_meta() {
+		global $post, $wpdb;
+		$meta_sql      = "SELECT meta_id, meta_key, meta_value FROM {$wpdb->prefix}postmeta WHERE post_id = $post->ID";
+		$download_meta = $wpdb->get_results( $meta_sql );
+		?>
+		<div class="inside">
+			<div style=>
+				<table class="wp-list-table widefat striped downloads">
+					<thead>
+					<tr>
+						<th><?php _e( 'ID', 'edd-dev-tools' ); ?></th>
+						<th><?php _e( 'Key', 'edd-dev-tools' ); ?></th>
+						<th><?php _e( 'Value', 'edd-dev-tools' ); ?></th>
+					</tr>
+					</thead>
+					<tfoot>
+					<tr>
+						<th><?php _e( 'ID', 'edd-dev-tools' ); ?></th>
+						<th><?php _e( 'Key', 'edd-dev-tools' ); ?></th>
+						<th><?php _e( 'Value', 'edd-dev-tools' ); ?></th>
+					</tr>
+					</tfoot>
+					<tbody>
+					<?php if ( ! empty( $download_meta ) ) : ?>
+						<?php foreach ( $download_meta as $meta ) : ?>
+							<tr>
+								<td><?php echo $meta->meta_id; ?></td>
+								<td><?php echo $meta->meta_key; ?></td>
+								<td>
+
+									<?php
+									if ( is_serialized( $meta->meta_value ) ) {
+										_e( 'Serialized Data', 'edd-dev-tools' );
+										?>
+										<span class="dashicons dashicons-visibility" title="<?php echo esc_attr( $meta->meta_value ); ?>"></span>
+										<p>
+											<?php edd_dev_tools()->print_pre( unserialize( $meta->meta_value ), false, true ); ?>
+										</p>
+										<?php
+									} else {
+										?><code style="white-space: normal"><?php echo $meta->meta_value; ?></code><?php
+									}
+									?>
+								</td>
+							</tr>
+						<?php endforeach; ?>
+					<?php else: ?>
+						<tr><td colspan="3"><?php printf( __( 'No Customer Meta Found', 'edd-dev-tools' ), edd_get_label_plural() ); ?></td></tr>
+					<?php endif; ?>
+					</tbody>
+				</table>
+			</div>
 		</div>
 		<?php
 	}
