45e10611a5bd6a5f44b55f78e99594841f6e0bfe
Chris Klosowski <cklosowski@gmail.com>
2018-07-16 15:58:24 -0700

    Update payment meta list for order meta table

--- includes/payments.php
+++ includes/payments.php
@@ -36,10 +36,10 @@ class EDD_DT_Payments {
 		ini_set( 'xdebug.var_display_max_data', 1024 );
 		?>
 		<div id="edd-payment-meta" class="postbox">
-			<h3 class="hndle"><?php _e( 'Payment Postmeta Items', 'edd-dev-tools' ); ?></h3>
+			<h3 class="hndle"><?php _e( 'Order Meta Items', 'edd-dev-tools' ); ?></h3>
 			<div class="inside">
 				<?php
-				$meta_sql     = "SELECT meta_id, meta_key, meta_value FROM {$wpdb->prefix}postmeta WHERE post_id = $payment_id";
+				$meta_sql     = "SELECT meta_id, meta_key, meta_value FROM {$wpdb->prefix}edd_ordermeta WHERE edd_order_id = $payment_id";
 				$payment_meta = $wpdb->get_results( $meta_sql );
 				?>
 				<div>
@@ -83,7 +83,7 @@ class EDD_DT_Payments {
 									</tr>
 								<?php endforeach; ?>
 							<?php else: ?>
-								<tr><td colspan="3"><?php printf( __( 'No Customer Meta Found', 'edd-dev-tools' ), edd_get_label_plural() ); ?></td></tr>
+								<tr><td colspan="3"><?php printf( __( 'No Order Meta Found', 'edd-dev-tools' ), edd_get_label_plural() ); ?></td></tr>
 							<?php endif; ?>
 						</tbody>
 					</table>
