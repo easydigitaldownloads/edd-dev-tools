efcefcfbfcd6584cbe0ccabde1abcec1264376e1
Chris Klosowski <cklosowski@gmail.com>
2019-03-20 17:44:39 -0700

    Add support for 3.x and 2.9.x payment meta

--- includes/payments.php
+++ includes/payments.php
@@ -25,7 +25,11 @@ class EDD_DT_Payments {
 	}
 
 	private function hooks() {
-		add_action( 'edd_view_order_details_main_after', array( $this, 'payment_post_meta' ), 99, 1 );
+		if ( version_compare( EDD_VERSION, '3.0.0-beta1', '<' ) ) {
+			add_action( 'edd_view_order_details_main_after', array( $this, 'legacy_payment_post_meta' ), 99, 1 );
+		} else {
+			add_action( 'edd_view_order_details_main_after', array( $this, 'payment_post_meta' ), 99, 1 );
+		}
 	}
 
 	public function payment_post_meta( $payment_id ) {
@@ -85,6 +89,71 @@ class EDD_DT_Payments {
 							<?php else: ?>
 								<tr><td colspan="3"><?php printf( __( 'No Order Meta Found', 'edd-dev-tools' ), edd_get_label_plural() ); ?></td></tr>
 							<?php endif; ?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+		<?php
+	}
+
+	public function legacy_payment_post_meta( $payment_id ) {
+		global $wpdb;
+
+		ini_set( 'xdebug.var_display_max_depth', 5 );
+		ini_set( 'xdebug.var_display_max_children', 256 );
+		ini_set( 'xdebug.var_display_max_data', 1024 );
+		?>
+		<div id="edd-payment-meta" class="postbox">
+			<h3 class="hndle"><?php _e( 'Payment Postmeta Items', 'edd-dev-tools' ); ?></h3>
+			<div class="inside">
+				<?php
+				$meta_sql     = "SELECT meta_id, meta_key, meta_value FROM {$wpdb->prefix}postmeta WHERE post_id = $payment_id";
+				$payment_meta = $wpdb->get_results( $meta_sql );
+				?>
+				<div>
+					<table class="wp-list-table widefat striped downloads">
+						<thead>
+						<tr>
+							<th><?php _e( 'ID', 'edd-dev-tools' ); ?></th>
+							<th><?php _e( 'Key', 'edd-dev-tools' ); ?></th>
+							<th><?php _e( 'Value', 'edd-dev-tools' ); ?></th>
+						</tr>
+						</thead>
+						<tfoot>
+						<tr>
+							<th><?php _e( 'ID', 'edd-dev-tools' ); ?></th>
+							<th><?php _e( 'Key', 'edd-dev-tools' ); ?></th>
+							<th><?php _e( 'Value', 'edd-dev-tools' ); ?></th>
+						</tr>
+						</tfoot>
+						<tbody>
+						<?php if ( ! empty( $payment_meta ) ) : ?>
+							<?php foreach ( $payment_meta as $meta ) : ?>
+								<tr>
+									<td><?php echo $meta->meta_id; ?></td>
+									<td><?php echo $meta->meta_key; ?></td>
+									<td>
+
+										<?php
+										if ( is_serialized( $meta->meta_value ) ) {
+											_e( 'Serialized Data', 'edd-dev-tools' );
+											?>
+											<span class="dashicons dashicons-visibility" title="<?php echo esc_attr( $meta->meta_value ); ?>"></span>
+											<p>
+												<?php edd_dev_tools()->print_pre( unserialize( $meta->meta_value ) ); ?>
+											</p>
+											<?php
+										} else {
+											?><code><?php echo $meta->meta_value; ?></code><?php
+										}
+										?>
+									</td>
+								</tr>
+							<?php endforeach; ?>
+						<?php else: ?>
+							<tr><td colspan="3"><?php printf( __( 'No Customer Meta Found', 'edd-dev-tools' ), edd_get_label_plural() ); ?></td></tr>
+						<?php endif; ?>
 						</tbody>
 					</table>
 				</div>
